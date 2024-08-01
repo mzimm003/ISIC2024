a1c9aa336ebe74b96a506686ab25734f88890961
mzimm003 <mzimmerman38@gatech.edu>
2024-08-01 06:00:46 +0000

    Update site, including plan. Intoduce preprocess.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 import numpy as np
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 import pandas as pd
 from PIL import Image
@@ -12,21 +13,51 @@ from pathlib import Path
 
 from torch import nn
 
+class Rescale(nn.Module):
+    def __init__(
+            self,
+            minimum=0,
+            maximum=255,
+            *args,
+            **kwargs) -> None:
+        """
+        Rescale inputs of a preset range to a range from 0 to 1.
+
+        Args:
+            minimum: the smallest possible value of expected inputs.
+            maximum: the largest possible value of expected inputs.
+        """
+        super().__init__(*args, **kwargs)
+        self.min = minimum
+        self.max = maximum
+    
+    def forward(self, x):
+        return (x - self.min) / (self.max - self.min)
+
 class PreProcess(nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.processes = {}
 
     def forward(self, x):
-        return nn.Sequential(*list(self._modules.values()))(x)
+        for proc in self._modules.values():
+            x = proc(x)
+        return x
 
 class PPInput(PreProcess):
+    def __init__(
+        self,
+        rescale_images = True,
+        *args,
+        **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.rescale = Rescale() if rescale_images else None
+
+class PPTarget(PreProcess):
     def __init__(
         self,
         *args,
         **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.normalize = nn.Norm
 
 class SkinLesions(Dataset):
     def __init__(self, annotations_file, img_file, img_dir, transform=None, target_transform=None):
@@ -50,8 +81,14 @@ class SkinLesions(Dataset):
         return image, label
 
 def main():
-    SkinLesions("train-metadata.csv", "train-image.hdf5", "train-image")
-
+    data = SkinLesions(
+        "train-metadata.csv",
+        "train-image.hdf5",
+        "train-image",
+        transform=PPInput(),
+        target_transform=PPTarget())
+    LDA = LinearDiscriminantAnalysis()
+    pass
 
 if __name__ == "__main__":
     main()
