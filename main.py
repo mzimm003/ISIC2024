fbec472d8daa996872201524b6e38189deb3d85f
mzimm003 <mzimmerman38@gatech.edu>
2024-08-05 02:23:54 +0000

    Introduce Onnx persistence

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ from sklearn.decomposition import (
 from sklearn.preprocessing import (
     OrdinalEncoder
 )
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+import onnxruntime
 
 import pandas as pd
 from PIL import Image
@@ -238,13 +241,34 @@ class Utilities:
     class DATASETS:
         SkinLesions = SkinLesions
 
-class FeatureReductionForTraining(Script):
+class TrainingScript(Script):
+    data: Type[Dataset]
+    save_path: Union[str, Path]
+    def save_model(self, model):
+        inp, target = next(iter(self.data))
+        onx = convert_sklearn(model, initial_types=[("X", FloatTensorType([None, inp.shape[-1]]))])
+        with open(self.save_path / "{}.onnx".format(str(model)), "wb") as f:
+            f.write(onx.SerializeToString())
+
+class ServingScript(Script):
+    data: Type[Dataset]
+    save_path: Union[str, Path]
+    def load_model(self, load_path):
+        class Model:
+            def __init__(slf) -> None:
+                slf.model = onnxruntime.InferenceSession(load_path)
+            def __call__(slf, x:pd.DataFrame, *args: Any, **kwds: Any) -> Any:
+                return slf.model.run(None, {"X":x.astype(np.float32).to_numpy()})
+        return Model()
+
+class FeatureReductionForTraining(TrainingScript):
     def __init__(
             self,
             dataset:Union[str, Utilities.DATASETS, Type[Dataset]] = None,
             dataset_kwargs:Dict[str, Any] = None,
             feature_reducer:Union[str, Utilities.FEATURE_REDUCERS] = None,
             feature_reducer_kwargs:Dict[str, Any] = None,
+            save_path:Union[str, Path] = None,
             **kwargs) -> None:
         """
         Args:
@@ -252,12 +276,14 @@ class FeatureReductionForTraining(Script):
             dataset_kwargs: Configuration for the dataset.
             feature_reducer: The feature reducer class to be used.
             feature_reducer_kwargs: Configuration for the feature reducer.
+            save_path: Specify a path to which feature reducer should be saved.
         """
         super().__init__(**kwargs)
         self.data = dataset
         self.ds_kwargs = dataset_kwargs if dataset_kwargs else {}
         self.feature_reducer = feature_reducer
         self.fr_kwargs = feature_reducer_kwargs if feature_reducer_kwargs else {}
+        self.save_path = save_path
     
     def setup(self):
         self.data = (
@@ -268,10 +294,12 @@ class FeatureReductionForTraining(Script):
             getattr(Utilities.FEATURE_REDUCERS, self.feature_reducer)
             if isinstance(self.feature_reducer, str)
             else self.feature_reducer)(**self.fr_kwargs)
+        self.save_path = Path(self.save_path)
 
     def run(self):
-        inp, tgt = self.data[:]        
+        inp, tgt = self.data[:]
         self.feature_reducer.fit(inp, tgt)
+        self.save_model(self.feature_reducer)
 
 class Main(Script):
     def __init__(self, **kwargs) -> None:
@@ -318,7 +346,8 @@ class Main(Script):
             feature_reducer = "PCA",
             feature_reducer_kwargs={
                 "n_components":.9999
-            }
+            },
+            save_path="./models/feature_reduction"
             )
         script.setup()
         script.run()
